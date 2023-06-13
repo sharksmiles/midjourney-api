8804f88650fbb99f083977da193dd545fcd58710
chenwu <chenwu@skieer.com>
2023-06-14 00:12:27 +0800

    node中间层处理

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,21 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import getImagineUrl from "./imagine";
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+app.get("/getImagineUrl", async (req: Request, res: Response) => {
+  try {
+    const { keyword } = req.query;
+    const msg = keyword ? await getImagineUrl(keyword) : "";
+    msg
+      ? res.send(msg)
+      : res.status(500).json({ error: "keyword is require!!!" });
+  } catch (error) {
+    console.error("Error while processing API requests:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });
